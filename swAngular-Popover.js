ef80083184fbba1be6ae4dbf0a9239394e9bc0b3
Ahmad <ahmad.cousin@zenika.com>
2015-11-13 14:21:57 +0100

    feat(popover): make the file es2015 compatible with module, let and arrow

--- swAngular-Popover.js
+++ swAngular-Popover.js
@@ -1,114 +1,106 @@
-(function () {
-    angular.module('swAngularPopover', [])
-        .service('swPopoverService', swPopoverService)
-        .directive('swAngularPopover', swAngularPopover);
+import angular from 'angular';
+
+export default angular.module('swAngularPopover', [])
+    .service('swPopoverService', swPopoverService)
+    .directive('swAngularPopover', swAngularPopover);
 
 
-    swPopoverService.$inject = [];
-    function swPopoverService() {
-        var self = this;
+swPopoverService.$inject = [];
+function swPopoverService() {
+    this.popoverRepository = {};
+    this.close = (id) => this.popoverRepository[id].popover('destroy');
+}
 
-        self.popoverRepository = {};
-        self.close = function (id) {
-            self.popoverRepository[id].popover('destroy');
-        };
-    }
-
-    swAngularPopover.$inject = ['swPopoverService', '$compile'];
-    function swAngularPopover(swPopoverService, $compile) {
-        return {
-            restrict: 'A',
-            scope: {
-                content: '@swAngularPopover',
-                url: '@swUrl',
-                options: '=swOptions',
-                popoverId: '@swPopoverId'
-            },
-            link: link
-        };
-
-        function link($scope, $element) {
-            swPopoverService.popoverRepository[$scope.popoverId] = $element;
-
-            var htmlParam = true;
-            var animationParam = true;
-            var placementParam = 'auto';
-            var triggerParam = 'click'; //click | hover | focus | manual
-            var titleParam = '';
-            var delayParam = 0;
-            var containerParam = false;
-
-            for (var fieldKey in $scope.options) {
-                switch (fieldKey) {
-                    case 'html':
-                        htmlParam = $scope.options.html;
-                        break;
-                    case 'animation':
-                        animationParam = $scope.options.animation;
-                        break;
-                    case 'placement':
-                        placementParam = $scope.options.placement;
-                        break;
-                    case 'trigger':
-                        triggerParam = $scope.options.trigger;
-                        break;
-                    case 'title':
-                        titleParam = $scope.options.title;
-                        break;
-                    case 'delay':
-                        delayParam = $scope.options.delay;
-                        break;
-                    case 'container':
-                        containerParam = $scope.options.container;
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            var contentElement;
-            if ($scope.url && $scope.url.length > 0) {
-                /*
-                 * need two capsulating elements
-                 * probably because:
-                 *  - contents() dismisses one layer
-                 *  - <div data-ng-include>-element is compiled to comment
-                 *  - popover dismisses comments without encapsulating html element
-                 */
-                contentElement = angular.element('<div><div><div data-ng-include="\'' + $scope.url + '\'"></div></div></div>');
-            } else {
-                contentElement = angular.element('<span>' + $scope.content + '</span>');
-            }
-            $compile(contentElement)($scope.$parent);
-
-            // Store contents
-            var contents = contentElement.contents();
-
-            var isShown = false;
-            $element.bind(triggerParam, function () {
-                if (!isShown) {
-
-                    $element.popover({
-                        html: htmlParam,
-                        content: contents,
-                        animation: animationParam,
-                        placement: placementParam,
-                        trigger: 'manual',
-                        title: titleParam,
-                        delay: delayParam,
-                        container: containerParam
-                    });
-                    $element.on('shown.bs.popover', function () {
-                        isShown = true;
-                    });
-                    $element.on('hidden.bs.popover', function () {
-                        isShown = false;
-                    });
-                    $element.popover('show');
-                } else {
-                    $element.popover('destroy');
-                }
-            });
+swAngularPopover.$inject = ['swPopoverService', '$compile'];
+function swAngularPopover(swPopoverService, $compile) {
+    return {
+        link,
+        restrict: 'A',
+        scope: {
+            content: '@swAngularPopover',
+            url: '@swUrl',
+            options: '=swOptions',
+            popoverId: '@swPopoverId'
         }
+    };
+
+    function link(scope, element) {
+        swPopoverService.popoverRepository[scope.popoverId] = element;
+
+        let htmlParam = true;
+        let animationParam = true;
+        let placementParam = 'auto';
+        let triggerParam = 'click'; //click | hover | focus | manual
+        let titleParam = '';
+        let delayParam = 0;
+        let containerParam = false;
+
+        let contentElement;
+
+        for (let fieldKey in scope.options) {
+            switch (fieldKey) {
+            case 'html':
+                htmlParam = scope.options.html;
+                break;
+            case 'animation':
+                animationParam = scope.options.animation;
+                break;
+            case 'placement':
+                placementParam = scope.options.placement;
+                break;
+            case 'trigger':
+                triggerParam = scope.options.trigger;
+                break;
+            case 'title':
+                titleParam = scope.options.title;
+                break;
+            case 'delay':
+                delayParam = scope.options.delay;
+                break;
+            case 'container':
+                containerParam = scope.options.container;
+                break;
+            default:
+                break;
+            }
+        }
+
+        if (scope.url && scope.url.length > 0) {
+            /*
+             * need two capsulating elements
+             * probably because:
+             *  - contents() dismisses one layer
+             *  - <div data-ng-include>-element is compiled to comment
+             *  - popover dismisses comments without encapsulating html element
+             */
+            contentElement = angular.element(`<div><div><div data-ng-include="'${scope.url}'"></div></div></div>`);
+        } else {
+            contentElement = angular.element(`<span>${scope.content}</span>`);
+        }
+        $compile(contentElement)(scope.$parent);
+
+        // Store contents
+        let contents = contentElement.contents();
+
+        let isShown = false;
+        element.bind(triggerParam, () => {
+            if (!isShown) {
+                element.popover({
+                    html: htmlParam,
+                    content: contents,
+                    animation: animationParam,
+                    placement: placementParam,
+                    trigger: 'manual',
+                    title: titleParam,
+                    delay: delayParam,
+                    container: containerParam
+                });
+                element.on('shown.bs.popover', () => isShown = true);
+                element.on('hidden.bs.popover', () => isShown = false);
+                element.popover('show');
+            } else {
+                element.popover('destroy');
+            }
+        });
     }
-}());
+}
